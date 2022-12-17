7826cce6757e054b4449e1efb813449e39fc122e
Mcdaid101 <matthewmcdaid1@hotmail.com>
2022-12-17 18:55:58 +0000

    added variables in js file for the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-console.log("hey")
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".button-open");
+const closeModalBtn = document.querySelector(".button-close");
+
