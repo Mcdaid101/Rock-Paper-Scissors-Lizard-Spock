cde7223bb939fb8bc4fa26250bbdecf434d1e0e2
Mcdaid101 <matthewmcdaid1@hotmail.com>
2022-12-17 18:58:28 +0000

    added open modal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector(".button-open");
 const closeModalBtn = document.querySelector(".button-close");
 
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+
+  openModalBtn.addEventListener("click", openModal);
