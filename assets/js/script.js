a8f517c3bcda2b1bdc20a68d2bcfd8a7321be7f2
Mcdaid101 <matthewmcdaid1@hotmail.com>
2022-12-18 14:37:17 +0000

    removed overlay function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
 const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector(".button-open");
 const closeModalBtn = document.querySelector(".button-close");
 
 const openModal = function () {
     modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
   };
 
   const closeModal = function() {
     modal.classList.add("hidden");
-    overlay.classList.add("hidden");
   }
 
   openModalBtn.addEventListener("click", openModal);
