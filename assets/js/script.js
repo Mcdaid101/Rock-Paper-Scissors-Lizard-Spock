37f974f347428847258356701243489e55bc2c73
Mcdaid101 <matthewmcdaid1@hotmail.com>
2022-12-19 21:09:12 +0000

    capitalized the variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,19 +34,19 @@ function computerGo() {
 
   switch(randNum){
     case 1:
-      computer = "rock";
+      computer = "Rock";
       break;
     case 2:
-      computer = "paper";
+      computer = "Paper";
       break;
     case 3:
-      computer = "scissors";
+      computer = "Scissors";
       break;
     case 4:
-      computer = "lizard";
+      computer = "Lizard";
         break;
     case 5:
-      computer = "spock";
+      computer = "Spock";
         break; 
   }
 }
@@ -59,81 +59,81 @@ function checkResult() {
   
   let winnerText = "";
 
-  if(player === "rock") {
-    if(computer === "lizard"){
+  if(player === "Rock") {
+    if(computer === "Lizard"){
       playerWins++;
       winnerText = "Rock crushes Lizard you win!";
-    } else if (computer === "scissors" ){
+    } else if (computer === "Scissors" ){
       playerWins++;
       winnerText = "Rock crushes Scissors you win!";
-    } else if (computer === "paper"){
+    } else if (computer === "Paper"){
       computerWins++;
       winnerText = "Paper covers Rock you lose!";
-    } else if (computer === "spock"){
+    } else if (computer === "Spock"){
       computerWins++;
       winnerText = "Spock vaporizes Rock you lose!";
     }
   }
 
-  if(player === "paper") {
-    if(computer === "rock"){
+  if(player === "Paper") {
+    if(computer === "Rock"){
       playerWins++;
       winnerText = "Paper covers Rock you win!";
-    } else if (computer === "spock" ){
+    } else if (computer === "Spock" ){
       playerWins++;
       winnerText = "Paper disproves Spock you win!";
-    } else if (computer === "scissors"){
+    } else if (computer === "Scissors"){
       computerWins++;
       winnerText = "Scissors cuts Paper you lose!";
-    } else if (computer === "lizard"){
+    } else if (computer === "Lizard"){
       computerWins++;
       winnerText = "Lizard eats Paper you lose!";
     }
   }
 
-  if(player === "scissors") {
-    if(computer === "rock"){
+  if(player === "Scissors") {
+    if(computer === "Rock"){
       computerWins++;
       winnerText = "Rock crushes Scissors you lose!";
-    } else if (computer === "spock" ){
+    } else if (computer === "Spock" ){
       computerWins++;
       winnerText = "Spock smashes Scissors you lose!";
-    } else if (computer === "paper"){
+    } else if (computer === "Paper"){
       playerWins++;
       winnerText = "Scissors cuts Paper you win!";
-    } else if (computer === "lizard"){
+    } else if (computer === "Lizard"){
       playerWins++;
       winnerText = "Scissors decapitates Lizard you win!";
     }
   }
 
-  if(player === "lizard") {
-    if(computer === "rock"){
+  if(player === "Lizard") {
+    if(computer === "Rock"){
       computerWins++;
       winnerText = "Rock crushes Lizard you lose!";
-    } else if (computer === "scissors" ){
+    } else if (computer === "Scissors" ){
       computerWins++;
       winnerText = "Scissors decapitates Lizard you lose!";
-    } else if (computer === "spock"){
+    } else if (computer === "Spock"){
       playerWins++;
       winnerText = "Lizard poisons Spock you win!";
-    } else if (computer === "paper"){
+    } else if (computer === "Paper"){
       playerWins++;
       winnerText = "Lizard eats Paper you win!";
     }
   }
 
-  if(player === "spock") {
-    if(computer === "rock"){
+  if(player === "Spock") {
+    if(computer === "Rock"){
       playerWins++;
       winnerText = "Spock vaporizes Rock you win!";
-    } else if (computer === "scissors" ){
+    } else if (computer === "Scissors" ){
       playerWins++;
       winnerText = "Spock smashes Scissors you win!";
-    } else if (computer === "lizard"){
+    } else if (computer === "Lizard"){
       computerWins++;
       winnerText = "Lizard poisons Spock you lose!";
-    } else if (computer === "paper"){
+    } else if (computer === "Paper"){
       computerWins++;
       winnerText = "Paper disproves Spock you lose!";
     }
