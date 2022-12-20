54475dc548cb5c723306f11a0e465e4ddfacaf15
Mcdaid101 <matthewmcdaid1@hotmail.com>
2022-12-20 13:56:04 +0000

    added no script tag and description and keyword tags

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,25 +1,24 @@
-document.forms['register-form'].onsubmit = function(event){
+
+document.forms['signup-form'].onsubmit = function(event){
 
     if(this.email.value.trim() === ""){
-        document.querySelector(".email-error").innerHTML = "Please enter a email";
+        document.querySelector(".email-error").innerHTML = "Please enter your email to continue";
         document.querySelector(".email-error").style.display = "block";
         event.preventDefault();
         return false;
      }
    
     if(this.username.value.trim() === ""){
-       document.querySelector(".username-error").innerHTML = "Please enter a username";
+       document.querySelector(".username-error").innerHTML = "Please enter a username to continue";
        document.querySelector(".username-error").style.display = "block";
        event.preventDefault();
        return false;
     }
   
     if(this.password.value.trim() === ""){
-       document.querySelector(".password-error").innerHTML = "Please enter a password";
+       document.querySelector(".password-error").innerHTML = "Please enter a password to continue";
        document.querySelector(".password-error").style.display = "block";
        event.preventDefault();
        return false;
     }
-  
-  
  }	
